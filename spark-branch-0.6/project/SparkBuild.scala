396766015d88f57973cc4b45f7206006c63f95f9
skycrab <yihaibobb@163.com>
2018-09-29 17:02:41 +0800

    add

--- spark-branch-0.6/project/SparkBuild.scala
+++ spark-branch-0.6/project/SparkBuild.scala
@@ -135,7 +135,8 @@ object SparkBuild extends Build {
       "colt" % "colt" % "1.2.0",
       "cc.spray" % "spray-can" % "1.0-M2.1",
       "cc.spray" % "spray-server" % "1.0-M2.1",
-      "org.apache.mesos" % "mesos" % "0.9.0-incubating"
+      "org.apache.mesos" % "mesos" % "0.9.0-incubating",
+      "voldemort.store.compress" % "h2-lzf" % "1.0"
     ) ++ (if (HADOOP_MAJOR_VERSION == "2") Some("org.apache.hadoop" % "hadoop-client" % HADOOP_VERSION) else None).toSeq,
     unmanagedSourceDirectories in Compile <+= baseDirectory{ _ / ("src/hadoop" + HADOOP_MAJOR_VERSION + "/scala") }
   ) ++ assemblySettings ++ extraAssemblySettings ++ Twirl.settings
